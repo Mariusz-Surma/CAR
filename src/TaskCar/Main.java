6b8f65791777f3564bf1f94e15a8747b46a4aeb8
Mariusz-Surma <surmariusz93@gmail.com>
2023-08-19 23:10:41 +0200

    5

--- src/TaskCar/Main.java
+++ src/TaskCar/Main.java
@@ -11,6 +11,6 @@ public class Main {
         System.out.println(car.isLightsOn());
         car.turnOnLights();
         System.out.println(car.isEngineOn());
-
+        car.turnOnLights();
     }
 }
